ec23fc14847090d4a5d68a71ef14c7fb264dd3a6
fukuda <fukudammk@gmail.com>
2019-02-05 01:48:10 +0900

    Create dockerfile and sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ docker stop libreria
 docker rm libreria
 docker build ./ -t libreria
 docker rmi $(docker images -f "dangling=true" -q)
-docker run --name libreria -it libreria ash
+docker run -v $(pwd)/app:/libreria/app  --name libreria -it libreria ash
