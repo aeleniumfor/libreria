618c4bcf777038cef79e082d47b9eb98e38dad67
fukuda <fukudammk@gmail.com>
2019-02-06 23:04:08 +0900

    Add sh

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM php:7.0-apache
 RUN curl -sS https://getcomposer.org/installer | php && \
     mv composer.phar /usr/local/bin/composer && \
     adduser libreria
-    
 RUN apt update 
 RUN apt install -y git zip unzip
 
--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
 docker stop libreria
-docker rm libreria
-docker build ./ -t libreria
-docker rmi $(docker images -f "dangling=true" -q)
-docker run -v $(pwd)/app:/var/www/html/libreria/app  --name libreria -it libreria bash
+# docker build ./ -t libreria
+dockerid=$(docker run --rm -p 8081:80 -v $(pwd)/app:/var/www/html/libreria/app --name libreria -d php:7.0-apache)
+echo $dockerid
+#
+docker exec $dockerid /bin/bash -c "curl -sS https://getcomposer.org/installer | php"
+docker exec $dockerid /bin/bash -c "adduser libreria"
+docker exec $dockerid /bin/bash -c "apt update && apt install -y git zip unzip && rm composer.phar"
+docker exec $dockerid /bin/bash -c "cd /var/www/html/libreria/app && composer install"
+docker exec -it $dockerid /bin/bash
