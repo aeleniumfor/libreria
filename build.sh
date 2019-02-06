63cb61b300114df14cbc02d02ec4747f976268e4
fukuda <fukudammk@gmail.com>
2019-02-06 23:18:00 +0900

    Add gitignore

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ docker stop libreria
 dockerid=$(docker run --rm -p 8081:80 -v $(pwd)/app:/var/www/html/libreria/app --name libreria -d php:7.0-apache)
 echo $dockerid
 #
-docker exec $dockerid /bin/bash -c "curl -sS https://getcomposer.org/installer | php"
+docker exec $dockerid /bin/bash -c "curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer"
 docker exec $dockerid /bin/bash -c "adduser libreria"
-docker exec $dockerid /bin/bash -c "apt update && apt install -y git zip unzip && rm composer.phar"
-docker exec $dockerid /bin/bash -c "cd /var/www/html/libreria/app && composer install"
+docker exec $dockerid /bin/bash -c "apt update && apt install -y git zip unzip"
+docker exec $dockerid /bin/bash -c "su libreria && cd /var/www/html/libreria/app && composer install"
 docker exec -it $dockerid /bin/bash
