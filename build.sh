ea0213a17eb73a9699272a16cf8c82f2f9c18727
fukuda <fukudammk@gmail.com>
2019-02-07 00:32:29 +0900

    Add git

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-if [ $0 -eq "build" ]; then 
+if [ $1 = "build" ]; then 
     docker stop libreria
-    dockerid=$(docker run --rm -p 8081:80 -v $(pwd)/app:/var/www/html/libreria/app --name libreria -d php:7.0-apache)
-    echo $dockerid
+    dockerid=$(docker run --rm -p 8081:80 -v $(pwd)/html:/var/www/html --name libreria -d php:7.0-apache)
     docker exec $dockerid /bin/bash -c "curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer"
     docker exec $dockerid /bin/bash -c "adduser libreria"
     docker exec $dockerid /bin/bash -c "apt update && apt install -y git zip unzip"
-    docker exec $dockerid /bin/bash -c "su libreria && cd /var/www/html/libreria/app && composer install"
-elif [$0 -eq "exec"]; then
+    docker exec $dockerid /bin/bash -c "su libreria && composer install"
+elif [ $1 = "exec" ]; then
+    dockerid=$(docker ps --filter name=libreria --format "{{.ID}}")
     docker exec -it $dockerid /bin/bash
-
+else
+    echo $dockerid
 fi
