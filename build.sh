9514210146b6f730296817635983c7734c8a88b7
fukuda <fukudammk@gmail.com>
2019-02-08 15:19:47 +0900

    Change run images

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ $1 = "build" ]; then 
     docker stop libreria
-    dockerid=$(docker run --rm -p 8081:80 -v $(pwd)/html:/var/www/html --name libreria -d php:7.0-apache)
+    dockerid=$(docker run --rm -p 8081:80 -v $(pwd)/html:/var/www/html --name libreria -d php:7-fpm)
     docker exec $dockerid /bin/bash -c "curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer"
     docker exec $dockerid /bin/bash -c "adduser libreria"
     docker exec $dockerid /bin/bash -c "apt update && apt install -y git zip unzip"
